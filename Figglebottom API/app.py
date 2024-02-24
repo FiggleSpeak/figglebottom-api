4eff43afb23f7434eb7fa196cd9bc25263192742
Dave Tan <64513649+Fasnon@users.noreply.github.com>
2024-02-24 13:24:19 +0800

    Update app.py

--- Figglebottom API/app.py
+++ Figglebottom API/app.py
@@ -1,6 +1,24 @@
 from flask import Flask
+import base64
+import random
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello, world!'
+
+
+
+@app.route('/evaluate_user', methods=['POST'])
+def score_user():
+    served_text = request.form.get('served_text')
+    user_audio = request.form.get('user_audio') 
+    var = random.randint()
+    
+    decoded = base64.decode(user_audio)
+    
+    with open("user_audio_" + var + ".wav", "w") as fo:
+        fo.write("decoded")
+        fo.close()
+    
+    
