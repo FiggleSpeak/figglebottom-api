4ea088965d2dbcb0050381cc01a89427c53e62a7
Lynus <limlynus@gmail.com>
2024-02-25 10:40:45 +0800

    no-tips line only appears when no tips are available for the whole word

--- Figglebottom API/app.py
+++ Figglebottom API/app.py
@@ -258,14 +258,14 @@ def score_user():
     for i in range(len(new_p2g)):
         # if new_p2g[i][1] == ' ':
         #     continue
-        if t.input_string[og_text_idx] != new_p2g[i][1][0]:
-            while t.input_string[og_text_idx] != new_p2g[i+1][1][0]:
-                new_p2g[i][1] += t.input_string[og_text_idx]
+        if sentence[og_text_idx] != new_p2g[i][1][0] and i-1 < len(new_p2g):
+            while sentence[og_text_idx] != new_p2g[i+1][1][0]:
+                new_p2g[i][1] += sentence[og_text_idx]
                 og_text_idx += 1
             new_p2g[i][1] = new_p2g[i][1][1:]
         else:
             og_text_idx += 1
-        if og_text_idx >= len(t.input_string):
+        if og_text_idx >= len(sentence):
             break
 
 
@@ -298,10 +298,13 @@ def score_user():
             words[-1].extend([1]*len(new_p2g[j][1]))
         else:
             words[-1].extend([0]*len(new_p2g[j][1]))
-            pronunciation_tips[-1].append(corrections.get(f'/{ARPAtoIPAchar(correct_seq[i])}/', 'Sorry, no tips yet!'))
+            pronunciation_tips[-1].append(corrections.get(f'/{ARPAtoIPAchar(correct_seq[i])}/', ''))
 
         j=j+1
     
+    for word_tips in pronunciation_tips:
+        if len(word_tips) > 0 and all([x == "" for x in word_tips]):
+            word_tips = ["Sorry, we don't have tips for this word yet!"]
 
     os.remove(wav_path)
 
